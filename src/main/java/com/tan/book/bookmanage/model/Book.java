74e9ce2b1c23d601ff28393066d965f431c68b14
luoyu396 <980644731@qq.com>
2019-11-06 16:17:19 +0800

    图书信息表

---
+++ src/main/java/com/tan/book/bookmanage/model/Book.java
@@ -0,0 +1,166 @@
+package com.tan.book.bookmanage.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 图书信息表
+ * @author tanbb
+ * @create 2019-11-06 16:05
+ */
+public class Book implements Serializable {
+
+    //图书ID
+    private String bookId;
+
+    //分类ID
+    private String typeId;
+
+    //图书名称
+    private String bookName;
+
+    //ISBN
+    private String ISBN;
+
+    //出版社
+    private String publisher;
+
+    //作者
+    private String author;
+
+    //页数
+    private int pageNum;
+
+    //出版时间
+    private Date publishDate;
+
+    //销售价格
+    private double salePrice;
+
+    //图书简介
+    private String introduction;
+
+    //折扣率
+    private double discount;
+
+    //销售数量
+    private int buyCount;
+
+    //图片id
+    private String imgId;
+
+    //创建时间
+    private Date createTime;
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(String typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(double salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public int getBuyCount() {
+        return buyCount;
+    }
+
+    public void setBuyCount(int buyCount) {
+        this.buyCount = buyCount;
+    }
+
+    public String getImgId() {
+        return imgId;
+    }
+
+    public void setImgId(String imgId) {
+        this.imgId = imgId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
