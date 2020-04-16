ced006c68e4241aeb864a3a342226c92a014816c
tanbb <980644731@qq.com>
2020-04-16 23:22:10 +0800

    去空行

--- src/main/java/com/tan/book/bookmanage/BookmanageApplication.java
+++ src/main/java/com/tan/book/bookmanage/BookmanageApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookmanageApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BookmanageApplication.class, args);
     }
-
 }
